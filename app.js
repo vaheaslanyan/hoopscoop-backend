74d7df7e96bbe2972c466a3bd0832c97018c39a3
Vahe Aslanyan <vahe.sde@gmail.com>
2020-02-15 15:10:15 -0800

    add frontend url to origins

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ app.use(bodyParser.json());
 app.use("/uploads/images", express.static(path.join("uploads", "images"))); //granting access to files from front end by building a new path pointing to the folder
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); //sets header to responses in order to avoid the browser CORS security error, the second argument indicates which urls should be allowed (star meaning any url)
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://hoopscoopfrontend.firebaseapp.com"
+  ); //sets header to responses in order to avoid the browser CORS security error, the second argument indicates which urls should be allowed (star meaning any url)
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization" //we set Content-Type and Authorization, the rest is automatically set by the browser
