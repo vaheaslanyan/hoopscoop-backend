2e5b81b88d9ec85e071474b93c045303c50378a9
Vahe Aslanyan <vahe.sde@gmail.com>
2020-02-20 20:36:29 -0800

    revert app.js changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const fs = require("fs"); // a node.js core module that allows us to interact with files
-//const path = require("path"); // built-in module
+const path = require("path"); // built-in module
 
 const express = require("express"),
   bodyParser = require("body-parser"),
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-//app.use("/uploads/images", express.static(path.join("uploads", "images"))); //granting access to files from front end by building a new path pointing to the folder
+app.use("/uploads/images", express.static(path.join("uploads", "images"))); //granting access to files from front end by building a new path pointing to the folder
 
 app.use((req, res, next) => {
   res.setHeader(
@@ -38,21 +38,22 @@ app.use((req, res, next) => {
   throw error;
 });
 
-// //this middleware will be only executed in case of an error
-// app.use((error, req, res, next) => {
-//   if (req.file) {
-//     //.file is a property added by multer and in this case we check if there is a file present while the error occured then we want to delete it
-//     fs.unlink(req.file.url, err => {
-//       console.log(err);
-//     }); //delets the file
-//   }
-//   if (res.headerSent) {
-//     //checking if a response has been sent
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occured" });
-// });
+//this middleware will be only executed in case of an error
+app.use((error, req, res, next) => {
+  if (req.file) {
+    //.file is a property added by multer and in this case we check if there is a file present while the error occured then we want to delete it
+    fs.unlink(req.file.path, err => {
+      //need to change path to url
+      console.log(err);
+    }); //delets the file
+  }
+  if (res.headerSent) {
+    //checking if a response has been sent
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occured" });
+});
 
 mongoose
   .connect(process.env.MONGO_URL)
