942e6bfec6678e862e8be1b68a0a82c122d2c5b4
Vahe Aslanyan <vahe.sde@gmail.com>
2020-02-22 13:03:59 -0800

    add url to header allow origin

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "*" //https://thehoopscoop-app.firebaseapp.com
+    "https://thehoopscoop-app.firebaseapp.com"
   ); //sets header to responses in order to avoid the browser CORS security error, the second argument indicates which urls should be allowed (star meaning any url)
   // res.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
   res.setHeader(
