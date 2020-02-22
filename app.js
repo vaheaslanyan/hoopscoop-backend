502122622c3221d12beb462dcde045f5b61fa7d7
Vahe Aslanyan <vahe.sde@gmail.com>
2020-02-21 16:51:16 -0800

    modify cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const fs = require("fs"); // a node.js core module that allows us to interact with files
-const path = require("path"); // built-in module
+//const path = require("path"); // built-in module
 
 const express = require("express"),
   bodyParser = require("body-parser"),
@@ -15,18 +15,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/uploads/images", express.static(path.join("uploads", "images"))); //granting access to files from front end by building a new path pointing to the folder
+//app.use("/uploads/images", express.static(path.join("uploads", "images"))); //granting access to files from front end by building a new path pointing to the folder
 
 app.use((req, res, next) => {
-  res.setHeader(
+  res.set(
     "Access-Control-Allow-Origin",
     "https://thehoopscoop-app.firebaseapp.com"
   ); //sets header to responses in order to avoid the browser CORS security error, the second argument indicates which urls should be allowed (star meaning any url)
-  res.setHeader(
+  res.set(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization" //we set Content-Type and Authorization, the rest is automatically set by the browser
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
 
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   if (req.file) {
     //.file is a property added by multer and in this case we check if there is a file present while the error occured then we want to delete it
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.url, err => {
       //need to change path to url
       console.log(err);
     }); //delets the file
