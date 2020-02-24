e8a87a0335bbdb7d921727d527b534fa101895dd
Vahe Aslanyan <vahe.sde@gmail.com>
2020-02-23 17:30:36 -0800

    commented out dotenv

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+//require("dotenv").config();
 
 const fs = require("fs"); // a node.js core module that allows us to interact with files
 //const path = require("path"); // built-in module
